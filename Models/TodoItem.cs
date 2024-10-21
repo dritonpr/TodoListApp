de55651b225d5d87913daa88c548a3ecb40b2b27
Driton Prushi <PC@DESKTOP-JT0BP1K>
2024-10-21 14:57:05 +0200

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
---
+++ Services/TodoService.cs
@@ -0,0 +1,24 @@
+﻿using TodoApp.Models;
+
+namespace TodoApp.Services
+{
+    public class TodoService
+    {
+        private readonly List<TodoItem> _todos = new List<TodoItem>();
+
+        public IReadOnlyList<TodoItem> GetTodos() => _todos;
+
+        public void AddTodo(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                _todos.Add(new TodoItem { Title = title });
+            }
+        }
+
+        public void ToggleTodoCompletion(TodoItem todo)
+        {
+            todo.IsCompleted = !todo.IsCompleted;
+        }
+    }
+}
